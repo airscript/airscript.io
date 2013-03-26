7c1ca65beb83e2da16f398f3aeef575a24fdbf17
Jeff Lindsay <progrium@gmail.com>
2013-03-25 21:45:16 -0700

    monkey patch

--- airscript/ui/__main__.py
+++ airscript/ui/__main__.py
@@ -6,5 +6,6 @@ import eventlet
 from airscript.ui import app
 
 if __name__ == "__main__":
+    eventlet.monkey_patch()
     wsgi.server(eventlet.listen(
         ('', int(os.environ.get("PORT", 5000)))), app)
